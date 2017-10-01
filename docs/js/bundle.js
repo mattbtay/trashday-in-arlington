2cbcfc5f2da5e24ece4c57d722c3f65b4ffa3ee9
Matt Taylor <mattbtay@gmail.com>
2017-09-30 19:13:20 -0500

    add copyright

--- docs/js/bundle.js
+++ docs/js/bundle.js
@@ -1477,7 +1477,7 @@ var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol
       };
     }).call(this, require("7YKIPe"), typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {}, require("buffer").Buffer, arguments[3], arguments[4], arguments[5], arguments[6], "/../node_modules/point-in-polygon/index.js", "/../node_modules/point-in-polygon");
   }, { "7YKIPe": 4, "buffer": 3 }] }, {}, [1]);
-}).call(this,require("7YKIPe"),typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {},require("buffer").Buffer,arguments[3],arguments[4],arguments[5],arguments[6],"/fake_9c021fb6.js","/")
+}).call(this,require("7YKIPe"),typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {},require("buffer").Buffer,arguments[3],arguments[4],arguments[5],arguments[6],"/fake_d80611a5.js","/")
 },{"7YKIPe":5,"base64-js":2,"buffer":4,"ieee754":6,"point-in-polygon":7}],2:[function(require,module,exports){
 (function (process,global,Buffer,__argument0,__argument1,__argument2,__argument3,__filename,__dirname){
 'use strict'
